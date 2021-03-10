c249689848265e688e2d92107727680c45c855ca
Raxit Majithiya <rax@expa.com>
2021-03-10 12:22:34 -0500

    Update iOS target SDK to iOS 9 to silence Xcode 12 warning

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ShowTime",
-    platforms: [.iOS("8.2")],
+    platforms: [.iOS(.v9)],
     products: [.library(name: "ShowTime", targets: ["ShowTime"])],
     targets: [.target(name: "ShowTime")],
     swiftLanguageVersions: [.v5]
